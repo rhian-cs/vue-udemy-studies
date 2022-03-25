d58fbfe4572ddabbd0cec4481a0f599cca0cdea4
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-03-25 13:29:59 -0300

    Add module 3

---
+++ 03-condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: "#desafio",
+  data: {
+    displayText1: true,
+    // array: ["Pedro", "Bia", "Ana", "Rebeca"],
+    touhouGames: [
+      "Embodiment of the Scarlet Devil",
+      "Perfect Cherry Blossom",
+      "Imperishable Night",
+      "Phantasmagoria of Flower View",
+      "Mountain of Faith",
+      "Subterranean Animism",
+      "Undefined Fantastic Object",
+      "Ten Desires",
+      "Double Dealing Character",
+      "Legacy of the Lunatic Kingdom",
+      "Hidden Star in Four Seasons",
+      "Wily Beast and the Weakest Creature",
+      "Unconnected Marketeers",
+    ],
+    book: {
+      title: "O Senhor dos Anéis",
+      author: "J.R.R. Tolkien",
+      volume: "3",
+    },
+    student: {
+      id: 10,
+      name: "Maria",
+      grades: [7.67, 8.33, 6.98, 9.21],
+    },
+  },
+});
