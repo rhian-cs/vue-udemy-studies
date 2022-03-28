ed12d2f8c6c7138794f80dfc3a469ce5199b848c
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-03-28 17:00:47 -0300

    Add new modules

---
+++ 06-vue-cli/01-projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app-component">
+    <div>Hello, Vue!</div>
+    <div>{{ title }}</div>
+    <button @click="title += '#'">###</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: "Test",
+    };
+  },
+};
+</script>
+
+<style>
+#app-component {
+  background-color: chocolate;
+  color: white;
+}
+</style>
