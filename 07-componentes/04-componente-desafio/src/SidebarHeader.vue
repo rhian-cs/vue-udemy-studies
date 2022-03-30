3a35bf6ee0a89619bdcf6aebd188ca2d15c02b25
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-03-30 17:35:14 -0300

    Finish module 7 challenge

---
+++ 07-componentes/04-componente-desafio/src/SidebarHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-toolbar flat>
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-title class="title"> Menu </v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-toolbar>
+</template>
