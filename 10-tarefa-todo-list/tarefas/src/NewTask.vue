3cd59254519b86b968e5c0110b888b9c48fe4a7b
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-06-30 14:37:46 -0300

    Start module 10 todo list app

---
+++ 10-tarefa-todo-list/tarefas/src/NewTask.vue
@@ -0,0 +1,14 @@
+<template>
+  <input v-bind="taskName"/>
+</template>
+
+<script>
+export default {
+  data() {
+    return { taskName: "" };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 10-tarefa-todo-list/tarefas/src/TaskManager.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Tarefas</h1>
+		<TaskCompletion />
+		<NewTask />
+		<TaskList />
+  </div>
+</template>
+
+<script>
+import TaskCompletion from "./TaskCompletion.vue";
+import NewTask from "./NewTask.vue";
+import TaskList from "./TaskList.vue";
+
+export default {
+  components: { TaskCompletion, NewTask, TaskList },
+  data() {
+    return { tasks: [] };
+  },
+  methods: {
+    addTask(task) {
+      return [...tasks, task];
+    },
+  },
+};
+</script>
+
+<style>
+</style>
